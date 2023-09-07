d08bb46c3a9a99723a054409401de66ae0012743
bk <belay.bura@gmail.com>
2023-09-07 23:44:44 +0300

    finished api tsks

--- task1/index.js
+++ task1/index.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 
-const response = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=1')
-const response2 = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=1')
-console.log(response.data)
-console.log(response2.data)
+// const response = await axios.get('https://jsonplaceholder.typicode.com/')
+// const response2 = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=1')
+// console.log(response.data)
+// console.log(response2.data)
+const  postData= async ()=>{
+    try{
+    const postResp = await axios.post('https://jsonplaceholder.typicode.com/posts', {title:"some title", body:"some body", userId:1})
+    console.log("--------------> ", postResp.data)
+    }catch(e){
+        console.log(e.message)
+    }
+    
+}
+postData()
+// console.log(postResp)
+// console.log(postResp.data.body)
